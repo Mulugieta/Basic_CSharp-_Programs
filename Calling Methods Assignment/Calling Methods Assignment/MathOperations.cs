2ba0e5ceff5d3a41dc7eeed67246664256272d99
Mulugieta <84230328+Mulugieta@users.noreply.github.com>
2025-09-10 11:49:36 -0400

    Calling Methods Assignment

---
+++ Calling Methods Assignment/Calling Methods Assignment/MathOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CallingMethodsAssignment
+{
+    // This class contains three math methods
+    public class MathOperations
+    {
+        // Method 1: Adds 10 to the number
+        public int AddTen(int number)
+        {
+            return number + 10;
+        }
+
+        // Method 2: Multiplies the number by 5
+        public int MultiplyByFive(int number)
+        {
+            return number * 5;
+        }
+
+        // Method 3: Subtracts 3 from the number
+        public int SubtractThree(int number)
+        {
+            return number - 3;
+        }
+    }
+}
---
+++ Calling Methods Assignment/Calling Methods Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the MathOperations class so we can use its methods
+            MathOperations mathOps = new MathOperations();
+
+            // Ask the user for a number
+            Console.WriteLine("Enter an integer to perform math operations on:");
+            int userNumber = Convert.ToInt32(Console.ReadLine()); // Convert input to integer
+
+            // Call the first method: AddTen
+            int addResult = mathOps.AddTen(userNumber);
+            Console.WriteLine($"{userNumber} + 10 = {addResult}");
+
+            // Call the second method: MultiplyByFive
+            int multiplyResult = mathOps.MultiplyByFive(userNumber);
+            Console.WriteLine($"{userNumber} * 5 = {multiplyResult}");
+
+            // Call the third method: SubtractThree
+            int subtractResult = mathOps.SubtractThree(userNumber);
+            Console.WriteLine($"{userNumber} - 3 = {subtractResult}");
+
+            // End program
+            Console.WriteLine("\nAll methods executed. Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
