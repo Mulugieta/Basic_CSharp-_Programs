5517680cd8dd9e7c4efd8e3dfc7990d4dcfa87ef
Mulugieta <84230328+Mulugieta@users.noreply.github.com>
2025-09-26 03:23:51 -0400

    Final Assignment Submission

---
+++ EFCodeFirstConsoleApp/EFCodeFirstConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EFCodeFirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentDatabaseContext())
+            {
+                var student = new Student()
+                {
+                    FirstName = "Mulugieta",
+                    LastName = "Elias",
+                    DateOfBirth = new DateOnly(2000, 8, 14),
+                    Major = "Computer Science",
+                    GPA = 3.85f
+
+                };
+                context.Students.Add(student);
+                context.SaveChanges();
+            }
+
+            Console.WriteLine("Press any key to exit the program");
+            Console.ReadKey();
+        }
+    }
+}
